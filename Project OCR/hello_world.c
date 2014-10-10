6b8569f7b0a63e26325ecfe057610a0486f1e3af
Xploow <vuuwilliam1@hotmail.fr>
2014-10-10 20:31:37 +0200

    test

---
+++ Project OCR/hello_world.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+ 
+// standard I/O needed for printf
+#include <stdio.h>
+ 
+// Main function
+int main()
+{
+  // The dummiest example
+  printf("Hello World !\n");
+ 
+  // always return something
+  // 0 is success
+  return 0;
+}
+
