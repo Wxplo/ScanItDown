ee0fad7f5a3b12dae4104344a6d1e52761709a08
Prescillia San <prescillia.san@epita.fr>
2014-10-26 18:36:52 +0100

    ocr_pr

---
+++ ocr_pr/Bloc.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include "segstruct.h"
+#include "pixel_operations.h"
+#include "pixel_operations.c"
+#include "filter.h"
+#include "filter.c"
+
+void Bloc(SDL_Surface *s);
